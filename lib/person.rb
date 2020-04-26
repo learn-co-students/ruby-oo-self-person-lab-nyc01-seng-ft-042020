3eb94c8e5bdf5a9734473cb2ab343409bd3d2b5f
Jessica <github email address>
2020-04-26 17:57:16 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,77 +1,77 @@
 # your code goes here
 require 'pry'
 class Person
-    attr_accessor :bank_account
-    attr_reader :name, :happiness, :hygiene, :friend
-    def initialize(name = nil)
+    attr_reader :hygiene, :happiness, :name, :friend
+    attr_accessor :bank_account 
+    
+    def initialize(name)
         @name = name
         @bank_account = 25
         @happiness = 8
-        @hygiene = 8
+        @hygiene = 8 
     end
 
     def happiness=(num)
-        if num < 0
-            @happiness = 0
-        elsif num > 10
+        if num > 10
             @happiness = 10
-        elsif
+        elsif num < 0
+            @happiness = 0
+        elsif 
             @happiness = num
         end
     end
 
     def hygiene=(num)
-        if num < 0
-            @hygiene = 0
-        elsif num > 10
+        if num > 10
             @hygiene = 10
-        elsif
+        elsif num < 0
+            @hygiene = 0
+        elsif 
             @hygiene = num
         end
+    end
+
+    def clean? 
+        @hygiene > 7
     end
 
     def happy?
         @happiness > 7
     end
 
-    def clean?
-        @hygiene > 7
-    end
-
     def get_paid(salary)
         @bank_account += salary
         "all about the benjamins"
     end
-
+            
     def take_bath
-        self.hygiene=(@hygiene + 4)
+        self.hygiene += 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        self.hygiene=(@hygiene - 3)
-        self.happiness=(@happiness + 2)
+        self.happiness += 2
+        self.hygiene -= 3
         "♪ another one bites the dust ♫"
     end
 
     def call_friend(friend)
-        self.happiness=(@happiness + 3)
-        friend.happiness = (friend.happiness + 3)
+        self.happiness += 3
+        friend.happiness = friend.happiness + 3
         "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
-    def start_conversation(person, topic)
+    def start_conversation(friend, topic)
         if topic == "politics"
-            self.happiness=(@happiness - 2)
-            person.happiness = person.happiness-2
-            return "blah blah partisan blah lobbyist"
+            self.happiness -= 2
+            friend.happiness = friend.happiness - 2
+            "blah blah partisan blah lobbyist"
         elsif topic == "weather"
-            self.happiness=(@happiness + 1)
-            person.happiness = person.happiness + 1
-            return "blah blah sun blah rain"
-        else
-            return "blah blah blah blah blah"
+            self.happiness += 1
+            friend.happiness = friend.happiness + 1
+            "blah blah sun blah rain"
+        else 
+            "blah blah blah blah blah"
         end
     end
-
 end
